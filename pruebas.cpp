1ac0a5b528f3b2affdb15251ce56ced8c802c586
anperezsa <andreysama@gmail.com>
2016-09-27 15:30:53 -0600

    mapeo directo aparte

---
+++ pruebas.cpp
@@ -0,0 +1,52 @@
+//pruebas.cpp
+// g++ -std=c++11 pruebas.cpp
+
+
+#include <iostream>
+//#include <iomanip>
+//#include <sstream>
+#include <random>
+
+int main()
+{
+  //mapeo directo
+  int cache_size = 256;
+  int block_size = 32;
+  int block_number;
+
+  block_number = cache_size / block_size;
+
+  std::cout << "block_number = " << block_number << std::endl;
+
+  int cache_blocks[block_number - 1];
+
+  int direccion_hex1 = 13;
+  //int direccion_hex2 = 44;
+  int position;
+
+  position = direccion_hex1%block_number;
+  std::cout << "position = " << position<< std::endl;
+
+  int hit = 0;
+  int miss = 0;
+
+  if (direccion_hex1 == cache_blocks[position])
+  {
+    hit = hit + 1;
+  }
+  else
+  {
+    cache_blocks[position] == direccion_hex1;
+    miss = miss + 1;
+  }
+
+  std::cout << "hits = " << hit << std::endl;
+  std::cout << "misses= " << miss << std::endl;
+
+  //int randNum = 0;
+  int randNum = rand() % 2; // generar 0 'o 1 random // a'un no sirve
+  std::cout << "random = " << randNum << std::endl;
+
+  return 0;
+
+}
